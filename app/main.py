74946e24c7d2ee38e73888456cca415e02b801df
Stanislav Berezhnyi <currystephh3@gmail.com>
2026-08-08 12:23:29 +0300

    Changes according to comments of AI and mentor

--- app/main.py
+++ app/main.py
@@ -5,9 +5,11 @@ class Cargo:
 
 class BaseRobot:
 
-    def __init__(self, name: str,
-                 weight: int,
-                 coords: list[int] = None) -> None:
+    def __init__(
+            self, name: str,
+            weight: int,
+            coords: list[int] | None = None
+    ) -> None:
         if coords is None:
             coords = [0, 0]
         self.name = name
@@ -32,7 +34,11 @@ class BaseRobot:
 
 class FlyingRobot(BaseRobot):
 
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(
+            self, name: str,
+            weight: int,
+            coords: list | None = None
+    ) -> None:
         if coords is None:
             coords = [0, 0, 0]
         super().__init__(name, weight, coords)
@@ -46,12 +52,13 @@ class FlyingRobot(BaseRobot):
 
 class DeliveryDrone(FlyingRobot):
 
-    def __init__(self,
-                 name: str,
-                 weight: int,
-                 max_load_weight: int,
-                 current_load: Cargo,
-                 coords: list = None) -> None:
+    def __init__(
+            self, name: str,
+            weight: int,
+            max_load_weight: int,
+            current_load: Cargo | None,
+            coords: list | None = None
+    ) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
